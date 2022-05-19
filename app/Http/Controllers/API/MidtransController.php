ea9ebbe2fa31bb5e10dfa178869423875d32f829
encepsuryana <encep.suryanajr@gmail.com>
2022-05-19 11:14:52 +0700

    update midtrans payment

---
+++ app/Http/Controllers/API/MidtransController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Midtrans\Config;
+use Midtrans\Notification;
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MidtransController extends Controller
+{
+    public function callback(Request $request)
+    {
+        //set configuration midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        //make intance midtrans notification
+        $notification = new Notification();
+
+        //assign to variable
+        $status = $notification->transaction_status;
+        $type = $notification->payment_type;
+        $fraud = $notification->fraud_status;
+        $order_id = $notification->order_id;
+
+        //find id transaction
+        $transaction = Transaction::findOrFail($order_id);
+
+        //handle notification
+        if ($status == 'capture') {
+            if ($type == 'creadit_card') {
+                if ($fraud == 'challange') {
+                    $transaction->status = 'PENDING';
+                } else {
+                    $transaction->status = 'SUCCESS';
+                }
+            }
+        } else if ($status == 'settlement') {
+            $transaction->status = 'SUCCESS';
+        } else if ($status == 'pending') {
+            $transaction->status = 'PENDING';
+        } else if ($status == 'deny') {
+            $transaction->status = 'CENCELED';
+        } else if ($status == 'expire') {
+            $transaction->status = 'CENCELED';
+        } else if ($status == 'cancel') {
+            $transaction->status = 'CENCELED';
+        }
+
+        //save transaction
+        $transaction->save();
+    }
+}
--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\API;
 
+use Exception;
+use Midtrans\Snap;
+use Midtrans\Config;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
-use App\Models\Transaction;
 use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
@@ -58,5 +61,70 @@ class TransactionController extends Controller
             $transaction,
             'Transaction updated'
         );
+    }
+
+    public function checkout(Request $request)
+    {
+        $request->validate([
+            'food_id' => 'required|exists:foods,id',
+            'user_id' => 'required|exists:users,id',
+            'quantity' => 'required',
+            'total' => 'required',
+            'status' => 'required',
+        ]);
+
+        $transaction = Transaction::create([
+            'food_id' => $request->food_id,
+            'user_id' => $request->user_id,
+            'quantity' => $request->quantity,
+            'total' => $request->total,
+            'status' => $request->status,
+            'payment_url' => ""
+        ]);
+
+        //configuration midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        //call transaction build
+        $transaction = Transaction::with(['food', 'user'])->find($transaction->id);
+
+        //make transaction midtrans
+        $midtrans = [
+            'transaction_details' => [
+                'order_id' => $transaction->id,
+                'gross_amount' => (int) $transaction->total,
+            ],
+            'customer_details' => [
+                'first_name' => $transaction->user->name,
+                'email' => $transaction->user->email,
+            ],
+            'enabled_payments' => ['dana', 'gopay', 'shopee_pay', 'bank_transfer'],
+            'vtweb' => [],
+        ];
+
+        //get midtrans
+        try {
+            //get page midtrans
+            $paymentUrl = Snap::createTransaction($midtrans)->redirect_url;
+
+            $transaction->paymant_url = $paymentUrl;
+            $transaction->save();
+
+            //return data to API
+            return ResponseFormatter::success(
+                $transaction,
+                'Transaction Success'
+            );
+        } catch (Exception $e) {
+            return ResponseFormatter::error(
+                $e->getMessage(),
+                'Transaction Failed',
+            );
+        }
+
+        //callback midtrans
     }
 }
