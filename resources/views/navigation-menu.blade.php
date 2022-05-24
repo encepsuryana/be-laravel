0b4e5eec6d291d34eeef58ad428901827e7430bf
encepsuryana <encep.suryanajr@gmail.com>
2022-05-24 14:27:06 +0700

    update transaction

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -22,6 +22,11 @@
                     </x-jet-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('transactions.index') }}" :active="request()->routeIs('transactions')">
+                        {{ __('Transactions') }}
+                    </x-jet-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('users.index') }}" :active="request()->routeIs('users')">
                         {{ __('Users') }}
                     </x-jet-nav-link>
